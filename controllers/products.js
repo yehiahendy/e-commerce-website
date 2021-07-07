4df5ed9ea1dccfd854151c9658af7ea1b42fb81c
yehiahendy <yayahendy11@gmail.com>
2021-07-07 14:22:35 +0200

    Add productById

--- controllers/products.js
+++ controllers/products.js
@@ -7,7 +7,6 @@ exports.creat = (req,res) => {
     const product = new Product(req.body);
     let form = new formidable.IncomingForm();
     form.keepExtensions = true;
-    console.log("aaaa");
     form.parse(req,(err,fields,files) => {
         if(err)
         {
@@ -15,7 +14,25 @@ exports.creat = (req,res) => {
             Error: "image can't be uploaded"
             });
         }
+        // make constrains to file size
+        //maximum file size = 1000000 =====> 1 mb 
+        if (files.photo.size > 1000000)
+        {
+            return res.status(400).json({
+                Error: "Sorry,can't upload this image"
+            });
+        }
+        // add some validation  
+        const {name,description,price,category,Quentity,shipping} = fields
+        if(!name || !description || !price || !category || !Quentity || !shipping )
+        {
+            return res.status(400).json({
+                Error: "All fields are rquired"
+            });
+        }
+        
         let product = new Product(fields);
+
         if (files.photo)
         {
             product.photo.data = fs.readFileSync(files.photo.path);
@@ -33,3 +50,20 @@ exports.creat = (req,res) => {
     });
 
 };
+/******************************************
+ * creat a middleware productById
+ *******************************************/
+exports.productById = (req,res,next,id) => {
+
+    Product.findById(id).exec((err,product) =>{
+        if(!product || err)
+        {
+            return res.status(400).json({
+                Error: "This product dose not exist"
+            });
+        }
+        req.product = product; 
+        next();
+    });
+
+}
--- routes/products.js
+++ routes/products.js
@@ -3,6 +3,12 @@ const router =  express.Router();
 const {creat} = require('../controllers/products');
 const {requireSignin,isAuth,isAdmin} = require('../controllers/auth')
 const {userById} =require('../controllers/user')
+const {productById} = require('../controllers/products')
 router.post("/product/creat/:userId",creat,requireSignin,isAuth,isAdmin);
+//test the productById
+router.get("/product/test/:productId",(req,res) =>{
+    res.json({Hi: "I'm still here"})
+});
 router.param("userId",userById);
+router.param("productId",productById);
 module.exports = router;
